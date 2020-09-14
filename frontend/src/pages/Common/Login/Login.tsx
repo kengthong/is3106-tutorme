482832bb1a1375fb8c30e32708c59672cac8c1a0
y1bs <39340177+y1bs@users.noreply.github.com>
2020-09-15 00:31:33 +0800

    Updated asset and creaeted preliminary login page
    Updates to assets logo.
    Updated login form with input fields and button. Pending logic updates

--- frontend/src/pages/Common/Login/Login.tsx
+++ frontend/src/pages/Common/Login/Login.tsx
@@ -1,26 +1,57 @@
 import React, { useEffect, useState } from "react";
 import { Button } from "primereact/button";
-import Header from "../../../components/Header/Header";
-import { getLorumIpsum } from "../../../actions/home";
+import { InputText } from "primereact/inputtext";
+import { Password } from "primereact/password";
 import BodyContainer from "../../../components/Layout/BodyContainer";
+import logo from "../../../assets/logo.jpg";
+
 const Login = () => {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    getLorumIpsum("https://baconipsum.com/api/?type=meat-and-filler").then(
-      (data) => {
-        setData(data);
-      }
-    );
-  }, []);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
   return (
     <div>
-      <Header />
       <BodyContainer>
-        <h1 style={{ fontSize: "3rem" }}>Welcome, log in</h1>
-        <Button label="Log in" className="p-button-raised p-button-rounded" />
-        <div>Data retrieved from lorum ipsum:</div>
-        <p>{data}</p>
-
+        <img src={logo} style={{ padding: 100 }} />
+        <h1
+          style={{
+            fontSize: "3rem",
+            padding: 50,
+          }}
+        >
+          Welcome, Please log in
+        </h1>
+        <form>
+          <span className="p-float-label p-field">
+            <InputText
+              id="emailinput"
+              value={email}
+              onChange={(e) => setEmail(e.toString)}
+            />
+            <label htmlFor="emailinput">Email</label>
+          </span>
+          <br></br>
+          <span className="p-float-label">
+            <Password
+              value={password}
+              onChange={(e) => setPassword(e.toString)}
+            />
+            <label htmlFor="password">Password</label>
+          </span>
+        </form>
+        <br></br>
+        <Button
+          label="Log in"
+          className="p-button-raised p-button-rounded"
+          //disabled={validateForm()}
+          //onClick={setSubmitted(true)}
+        />
+        <Button label="Register" className="p-button-link" />{" "}
       </BodyContainer>
     </div>
   );
