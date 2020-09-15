945589f3c25a299d5442116f416df948efd86460
y1bs <39340177+y1bs@users.noreply.github.com>
2020-09-15 22:38:30 +0800

    Updated Registration
    Registration page is done.
    Pending Logic update on Registration page
    
    Yarn add react-transition-group: To support primereact Calendar

--- frontend/src/components/Login/Registration.tsx
+++ frontend/src/components/Login/Registration.tsx
@@ -11,10 +11,12 @@ const Registration = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
-  const [gender, setGender] = useState("");
+  const [gender, setGender] = useState("Male");
   const [dob, setdob] = useState(new Date());
+  const [phone, setPhone] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
+  //Need help with this validation too
   function validateForm() {
     return email.length > 0 && password.length > 0;
   }
@@ -22,10 +24,21 @@ const Registration = () => {
     return password == password2;
   };
 
+  //To be implemented
   const handleSubmit = (e: any) => {
-    //Should call on
+    //Should call on API
     //Skeleton
     console.log("Submitted");
+  };
+
+  const handlefirstnameChange = (e: any) => {
+    const value = e && e.target && e.target.value ? e.target.value : "";
+    setfirstname(value);
+  };
+
+  const handlelastnameChange = (e: any) => {
+    const value = e && e.target && e.target.value ? e.target.value : "";
+    setlastname(value);
   };
 
   const handleEmailChange = (e: any) => {
@@ -38,22 +51,50 @@ const Registration = () => {
     setPassword(value);
   };
 
+  //Need some updates to this
   const handleRetypePasswordChange = (e: any) => {
     const value = e && e.target && e.target.value ? e.target.value : "";
     setPassword2(value);
     verifyPassword(value);
   };
 
+  const handlePhoneNumChange = (e: any) => {
+    const value = e && e.target && e.target.value ? e.target.value : "";
+    setPhone(value);
+  };
+
+  const handleGenderChange = (e: any) => {
+    const value = e && e.target && e.target.value ? e.target.value : "";
+    setGender(value);
+  };
+
+  const handleDateChange = (e: any) => {
+    const value = e && e.target && e.target.value ? e.target.value : "";
+    setdob(e);
+  };
+
   return (
     <form onSubmit={handleSubmit}>
       <div className="p-fluid ">
         <div className="p-field p-col-12 p-md-6">
-          <label htmlFor="firstname2">First name</label>
-          <InputText id="firstname2" type="text" />
+          <label htmlFor="firstname">First name</label>
+          <InputText
+            id="firstname"
+            type="text"
+            keyfilter="alphanum"
+            onChange={(e) => handlefirstnameChange(e)}
+            required={true}
+          />
         </div>
         <div className="p-field p-col-12 p-md-6">
-          <label htmlFor="lastname2">Last name</label>
-          <InputText id="lastname2" type="text" />
+          <label htmlFor="lastname">Last name</label>
+          <InputText
+            id="lastname"
+            type="text"
+            keyfilter="alphanum"
+            onChange={(e) => handlelastnameChange(e)}
+            required={true}
+          />
         </div>
 
         <div className="p-field ">
@@ -61,7 +102,9 @@ const Registration = () => {
           <InputText
             id="emailinput"
             type="text"
+            keyfilter="email"
             onChange={(e) => handleEmailChange(e)}
+            required={true}
           />
         </div>
         <div className="p-field ">
@@ -70,6 +113,7 @@ const Registration = () => {
             id="password"
             type="text"
             onChange={(e) => handlePasswordChange(e)}
+            required={true}
           />
         </div>
         <div className="p-field ">
@@ -78,25 +122,46 @@ const Registration = () => {
             id="password"
             type="text"
             onChange={(e) => handleRetypePasswordChange(e)}
+            required={true}
           />
         </div>
         <div className="p-field ">
           <label htmlFor="phonenumber">Phone Number</label>
-          <InputText id="phonenumber" type="text" />
+          <InputText
+            id="phone"
+            type="text"
+            keyfilter="int"
+            onChange={(e) => handlePhoneNumChange(e)}
+            required={true}
+          />
         </div>
         <div className="p-field ">
           <label htmlFor="gender">Gender</label>
         </div>
         <div className="p-field-radiobutton">
-          <RadioButton value="gen1" name="gender" />
+          <RadioButton
+            value="Male"
+            name="gender"
+            onChange={(e) => handleGenderChange(e)}
+            checked={gender === "Male"}
+          />
           <label htmlFor="gen1">Male</label>
         </div>
         <div className="p-field-radiobutton">
-          <RadioButton value="gen2" name="gender" />
+          <RadioButton
+            value="Female"
+            name="gender"
+            onChange={(e) => handleGenderChange(e)}
+            checked={gender === "Female"}
+          />
           <label htmlFor="gen2">Female</label>
         </div>
         <div className="p-field ">
           <label htmlFor="dob">Date of Birth</label>
+          <Calendar
+            onChange={(e) => handleDateChange({ date: e.value })}
+            required={true}
+          ></Calendar>
         </div>
       </div>
 
