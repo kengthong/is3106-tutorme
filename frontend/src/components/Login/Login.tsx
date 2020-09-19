8a9dbaed11a1c6a5f3f0cd779607608a10899f43
y1bs <39340177+y1bs@users.noreply.github.com>
2020-09-19 21:11:45 +0800

    Updated Login Page
    Updated with AntD CSS

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState, Component, FormEvent } from "react";
-import { Button } from "primereact/button";
-import { InputText } from "primereact/inputtext";
 import { useHistory } from "react-router-dom";
 import { REGISTRATION_URL } from "../../config/constants";
+import logo from "../../assets/logo.jpg";
+import { Input, Button } from "antd";
 
 const Login = () => {
   const history = useHistory();
-  const handleRedirectToLogin = () => history.push(REGISTRATION_URL);
+  const handleRedirectToRegister = () => history.push(REGISTRATION_URL);
 
   const [formData, setFormData] = useState({
     email: "5",
@@ -32,44 +32,59 @@ const Login = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="p-fluid">
-        <div className="p-field">
-          <label htmlFor="email">Email</label>
-          <InputText
-            name="email"
-            id="emailinput"
-            keyfilter="email"
-            type="text"
-            onChange={(e) => handleChange(e)}
-          />
+    <div>
+      <img src={logo} style={{ padding: 100 }} />
+      <h1
+        style={{
+          fontSize: "3rem",
+          padding: 50,
+        }}
+      >
+        Welcome, Please log in
+      </h1>
+
+      <form onSubmit={handleSubmit}>
+        <div className="p-fluid">
+          <div className="p-field">
+            <label htmlFor="email">Email</label>
+            <Input
+              name="email"
+              id="emailinput"
+              type="text"
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
+          <div className="p-field">
+            <label htmlFor="password">Password</label>
+            <Input.Password
+              name="password"
+              id="passwordinput"
+              type="password"
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
+          <br></br>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <Button
+              type="primary"
+              className="p-button-raised p-pt-4"
+              onClick={handleSubmit}
+            >
+              Login
+            </Button>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <Button
+              type="link"
+              className="p-button-link"
+              onClick={handleRedirectToRegister}
+            >
+              Registration
+            </Button>
+          </div>
         </div>
-        <div className="p-field">
-          <label htmlFor="password">Password</label>
-          <InputText
-            name="password"
-            id="passwordinput"
-            type="password"
-            onChange={(e) => handleChange(e)}
-          />
-        </div>
-        <br></br>
-        <div>
-          <Button
-            label="Login"
-            className="p-button-raised p-pt-4"
-            onClick={handleSubmit}
-          />
-        </div>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <Button
-            label="Login"
-            className="p-button-link"
-            onClick={handleRedirectToLogin}
-          />
-        </div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 };
 
--- frontend/src/pages/Common/Login/Login.tsx
+++ frontend/src/pages/Common/Login/Login.tsx
@@ -1,22 +1,12 @@
 import React from "react";
 
 import BodyContainer from "../../../components/Layout/BodyContainer";
-import logo from "../../../assets/logo.jpg";
 import LoginComponent from "../../../components/Login/Login";
 
 const Login = () => {
   return (
     <div>
       <BodyContainer>
-        <img src={logo} style={{ padding: 100 }} />
-        <h1
-          style={{
-            fontSize: "3rem",
-            padding: 50,
-          }}
-        >
-          Welcome, Please log in
-        </h1>
         <LoginComponent />
       </BodyContainer>
     </div>
