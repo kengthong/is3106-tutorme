1841a8f113e916ff55dbd10a0ffef7d2a247e763
Tay Zhi Hong, Owen <56811535+tayzhihong@users.noreply.github.com>
2020-09-23 11:35:27 +0800

    working tutor resource

--- backend/tutorme/tutorme-war/src/java/webservices/restful/TutorResource.java
+++ backend/tutorme/tutorme-war/src/java/webservices/restful/TutorResource.java
@@ -6,18 +6,32 @@
 package webservices.restful;
 
 import entity.Tutor;
+import enumeration.CitizenshipEnum;
+import enumeration.GenderEnum;
+import enumeration.QualificationEnum;
+import enumeration.RaceEnum;
 import exception.TutorNotFoundException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
-import javax.ejb.EJB;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.json.Json;
 import javax.json.JsonObject;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import session.RatingSessionLocal;
 import session.TutorSessionLocal;
 
 /**
@@ -29,8 +43,12 @@ import session.TutorSessionLocal;
 @Path("/tutor")
 public class TutorResource {
 
-    @EJB
-    TutorSessionLocal tutorSession;
+//    @Inject 
+//    RatingSessionLocal ratingSession;
+    RatingSessionLocal ratingSession = lookupRatingSessionLocal();
+//    @Inject 
+//    TutorSessionLocal tutorSession;
+    TutorSessionLocal tutorSession = lookupTutorSessionLocal();
 
     /**
      * Creates a new instance of TutorResource
@@ -48,39 +66,105 @@ public class TutorResource {
     @Path("/getTutors")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getTutors() {
-        List<Tutor> toReturn = null;
-        toReturn = tutorSession.retrieveAllTutors();
-        if (toReturn != null) {
-            GenericEntity<List<Tutor>> result = new GenericEntity<List<Tutor>>(toReturn) {
+        System.out.println("Getting tutors... ");
+        List<Tutor> tutors = new ArrayList();
+        tutors = tutorSession.retrieveAllTutors();
+        if (!tutors.isEmpty()) {
+            for (Tutor t : tutors) {
+                t.setSalt(null);
+                t.setPassword(null);
+                t.setMessages(null);
+                t.setJobListings(null);
+            }
+            GenericEntity<List<Tutor>> packet = new GenericEntity<List<Tutor>>(tutors) {
             };
-            return Response.status(200).entity(result).build();
+            return Response.status(200).entity(packet).type(MediaType.APPLICATION_JSON).build();
         } else {
-            JsonObject exception = Json.createObjectBuilder().add("error", "returned null value from REST/getTutors").build();
+            JsonObject exception = Json.createObjectBuilder().add("error", "returned empty list from REST/getTutors").build();
             return Response.status(400).entity(exception).build();
         }
-//        System.out.println("hello world");
 
     }
 
     @GET
-    @Path("/getTutorById")
+    @Path("/tutorProfile")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getTutorById(@QueryParam("tutorId") Long tutorId) {
-        Tutor toReturn = null;
+        System.out.println("Tutor Id is... " + tutorId);
         try {
-            toReturn = tutorSession.retrieveTutorById(tutorId);
+            Tutor result = tutorSession.retrieveTutorById(tutorId);
+            result.setPassword(null);
+            result.setSalt(null);
+            result.setMessages(null);
+            result.setJobListings(null);
+            GenericEntity<Tutor> packet = new GenericEntity<Tutor>(result) {
+            };
+            return Response.status(200).entity(packet).type(MediaType.APPLICATION_JSON).build();
         } catch (TutorNotFoundException ex) {
             JsonObject exception = Json.createObjectBuilder().add("error", "tutorId does not exists").build();
             return Response.status(400).entity(exception).build();
         }
-        if (toReturn != null) {
-            GenericEntity<Tutor> result = new GenericEntity<Tutor>(toReturn) {
+    }
+
+    @POST
+    @Path("/tutorProfile")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateTutorById(@QueryParam("tutorId") Long tutorId,
+            @QueryParam("firstName") String firstName,
+            @QueryParam("lastName") String lastName,
+            @QueryParam("mobileNum") String mobileNum,
+            @QueryParam("gender") String gender,
+            @QueryParam("dob") String dob,
+            @QueryParam("highestQualification") String highestQualification,
+            @QueryParam("citizenship") String citizenship,
+            @QueryParam("race") String race,
+            @QueryParam("profileDesc") String profileDesc
+    ) {
+        System.out.println("Updating Tutor Id is ... " + tutorId);
+        try {
+            GenderEnum genderEnum = gender.equals('M') ? GenderEnum.MALE : GenderEnum.FEMALE;
+            QualificationEnum qualiEnum = QualificationEnum.valueOf(QualificationEnum.class, highestQualification.toUpperCase());
+            CitizenshipEnum citiEnum = CitizenshipEnum.valueOf(CitizenshipEnum.class, citizenship.toUpperCase());
+            RaceEnum raceEnum = RaceEnum.valueOf(RaceEnum.class, race.toUpperCase());
+            SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-YYYY");
+            Date parsedDob = new Date();
+            try {
+                parsedDob = formatter.parse(dob);
+            } catch (ParseException ex) {
+                ex.printStackTrace();
+            }
+
+            Tutor result = tutorSession.updateTutorProfile(tutorId, firstName, lastName, mobileNum, genderEnum, parsedDob, qualiEnum, citiEnum, raceEnum, profileDesc);
+            result.setPassword(null);
+            result.setSalt(null);
+            result.setMessages(null);
+            result.setJobListings(null);
+            GenericEntity<Tutor> packet = new GenericEntity<Tutor>(result) {
             };
-            return Response.status(200).entity(result).build();
-        } else {
-            JsonObject exception = Json.createObjectBuilder().add("error", "returned null value from REST/getTutorById").build();
+            return Response.status(200).entity(packet).type(MediaType.APPLICATION_JSON).build();
+        } catch (TutorNotFoundException ex) {
+            JsonObject exception = Json.createObjectBuilder().add("error", "tutorId does not exists").build();
             return Response.status(400).entity(exception).build();
         }
     }
 
+    private TutorSessionLocal lookupTutorSessionLocal() {
+        try {
+            Context c = new InitialContext();
+            return (TutorSessionLocal) c.lookup("java:global/tutorme/tutorme-ejb/TutorSession!session.TutorSessionLocal");
+        } catch (NamingException ne) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
+    }
+
+    private RatingSessionLocal lookupRatingSessionLocal() {
+        try {
+            Context c = new InitialContext();
+            return (RatingSessionLocal) c.lookup("java:global/tutorme/tutorme-ejb/RatingSession!session.RatingSessionLocal");
+        } catch (NamingException ne) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
+    }
 }
