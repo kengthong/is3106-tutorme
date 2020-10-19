214400c511c300e8b6519c06343151cc6edbbbbb
kengthong <wookengthong@gmail.com>
2020-10-19 17:43:17 +0800

    integrated jwt into frontend. Added cors filter to backend

---
+++ frontend/src/services/Tutor.ts
@@ -0,0 +1,5 @@
+export class TutorService {
+    getLorumIpsumAPI() {
+        return null;
+    }
+};
---
+++ frontend/src/services/User.ts
@@ -0,0 +1,46 @@
+import jwt from 'jwt-decode'
+import {AUTHENTICATION_ERROR, BACKEND_BASE_URL, LOGIN_SUCCESSFUL, LOGOUT_SUCCESSFUL} from "../config/constants";
+import { Dispatch } from "redux"
+import { store } from "../store";
+import {Utility} from "../config/Utility";
+
+const jsonHeader= {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+};
+export class UserService {
+    static async login (email: string, password: string) {
+        const url = BACKEND_BASE_URL + '/person/login';
+        const body = { email, password };
+        await Utility.fetchBuilder(url, 'POST', jsonHeader, body)
+            .then(async (res) => {
+                if(res.ok) {
+                    const token = await res.text();
+                    const user = jwt(token);
+                    localStorage.setItem("token", token);
+                    store.dispatch({
+                        type: LOGIN_SUCCESSFUL,
+                        payload: user
+                    })
+                } else {
+                    const body = await res.text();
+                    store.dispatch({
+                        type: AUTHENTICATION_ERROR,
+                        payload: body
+                    })
+                }
+            })
+    }
+
+    static register() {
+
+    }
+
+    static logout() {
+        localStorage.removeItem("token");
+        store.dispatch({
+            type: LOGOUT_SUCCESSFUL,
+            payload: undefined
+        })
+    }
+}
