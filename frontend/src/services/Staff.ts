0414de111f7d713ec33c80b0b9a06148b68bcfa6
y1bs <39340177+y1bs@users.noreply.github.com>
2020-11-14 03:01:31 +0800

    Staff dashboard
    Chat available for staff on dashboardd already

--- frontend/src/services/Staff.ts
+++ frontend/src/services/Staff.ts
@@ -157,4 +157,68 @@ export class StaffService {
         }
     }
 
+
+    public static async getChatList(userId: number): Promise<chatListResponseType> {
+        const token = localStorage.getItem("staffToken");
+        const url = "http://localhost:8080/tutorme-war/webresources/message/conversations?personId=" + userId;
+        const header = {
+            ...jsonHeader,
+            'Authorization': 'Bearer ' + token
+        }
+
+        const response = await Utility.fetchBuilder(url, 'GET', header, null);
+        if(response.ok) {
+            return await response.json();
+        } else {
+            console.log(response);
+            return []
+        }
+
+    }
+
+    public static async getMessagesByUserIds(currentUserId: string, chatUserId: string): Promise<chatMessagesType> {
+        const url = "http://localhost:8080/tutorme-war/webresources/message/conversation?" + new URLSearchParams({
+            //p1Id: currentUserId, -----TODO: DOUBLE CHECK PLS!!!
+            p2Id: chatUserId
+        });
+        const token = localStorage.getItem("staffToken");
+        let data = [];
+        if(token) {
+            const header = {
+                ...jsonHeader,
+                'Authorization': 'Bearer ' + token
+            }
+
+            const response = await Utility.fetchBuilder(url, 'GET', header, null);
+            if(response.ok) {
+                data = await response.json();
+            } else {
+                console.log(response);
+            }
+        }
+        return data;
+    }
+
+    public static async sendMessage(senderId: string, receiverId: string, body: string): Promise<boolean> {
+        const url = "http://localhost:8080/tutorme-war/webresources/message/sendMessage";
+        const token = localStorage.getItem("staffToken");
+        if(token) {
+            const header = {
+                ...jsonHeader,
+                'Authorization': 'Bearer ' + token
+            }
+            const fetchBody = {
+                senderId,
+                receiverId,
+                body
+            };
+            const res = await Utility.fetchBuilder(url, 'POST', header, fetchBody);
+            console.log('res = ', res)
+            if(res.ok) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
