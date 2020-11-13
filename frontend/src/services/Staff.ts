3da58745ef942541118da7b23c11ad8b88d2cb13
Brandon <45054809+njynjynjy@users.noreply.github.com>
2020-11-14 03:17:43 +0800

    modal for staff table

--- frontend/src/services/Staff.ts
+++ frontend/src/services/Staff.ts
@@ -221,23 +221,6 @@ export class StaffService {
 
         return false;
     }
-
-    static async getAllTutees() {
-        const url = BACKEND_BASE_URL + '/staff/getTutees';
-        const token = localStorage.getItem("staffToken");
-        const jsonHeader = Utility.getJsonHeader();
-        const header = {
-            ...jsonHeader,
-            "Authorization": "Bearer " + token
-        };
-        const response = await Utility.fetchBuilder(url, 'GET', header, null);
-        if (response.ok) {
-            return await response.json();
-        } else {
-            return false;
-        }
-    }
-
     static async createNewStaff(params: createStaffParams): Promise<boolean> {
         const url = BACKEND_BASE_URL + "/person/register";
         const token = localStorage.getItem("staffToken");
