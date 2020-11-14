c5e04528688fb8a6be6255a07394b12680892ae4
Tay Zhi Hong, Owen <56811535+tayzhihong@users.noreply.github.com>
2020-11-14 10:23:26 +0800

    Fixed admin offer crash due to null rating

--- frontend/src/Staff/components/StaffView/Offers.tsx
+++ frontend/src/Staff/components/StaffView/Offers.tsx
@@ -1,132 +1,134 @@
-import React, {useEffect, useState} from 'react';
-import {Button, Table, Tag} from "antd";
-import moment from 'antd/node_modules/moment';
-import {JobListingService} from "../../../services/JobListing";
-import { useHistory } from 'react-router-dom';
-import {StaffService} from "../../../services/Staff";
-
-
+import React, { useEffect, useState } from "react";
+import { Button, Table, Tag } from "antd";
+import moment from "antd/node_modules/moment";
+import { JobListingService } from "../../../services/JobListing";
+import { useHistory } from "react-router-dom";
+import { StaffService } from "../../../services/Staff";
 
 const OfferListingTable = () => {
-    const [tableData, setTableData] = useState([])
-    const history = useHistory();
-    const getAllOffers = async () => {
-        const data = await StaffService.getOfferList();
-        setTableData(data)
-    }
-    useEffect(() => {
-        getAllOffers()
-    },[])
+	const [tableData, setTableData] = useState([]);
+	const history = useHistory();
+	const getAllOffers = async () => {
+		const data = await StaffService.getOfferList();
+		setTableData(data);
+	};
+	useEffect(() => {
+		getAllOffers();
+	}, []);
 
-    const viewJobListing = (id: number) => {
-        history.push('/job?id=' + id);
-    }
+	const viewJobListing = (id: number) => {
+		history.push("/job?id=" + id);
+	};
 
-    const _columns = [
-        {
-            title: 'Offer Status',
-            dataIndex: 'offerStatus',
-            render: (offerStatus: string) => {
-                const color = offerStatus === "ACCEPTED"? "success" : offerStatus === "REJECTED"? "error" : offerStatus === "WITHDRAW"? "default": "warning";
-                return (
-                    <span>
-                        <Tag color={color}>{offerStatus}</Tag>
-                    </span>
-                )
-            }
-        },
-        {
-            title: 'ID',
-            dataIndex: 'offerId',
-        },
-        {
-            title: 'Subject',
-            dataIndex: 'chosenSubject',
-            render: (subject: any) => {
-                return (
-                    <span>
-                    {subject.subjectName}
-                </span>
-                )
-            }
-        },
-        {
-            title: 'Created Date',
-            dataIndex: 'createdDate',
-            render: (date: string) => {
-                return (<span>{moment(date.split('[')[0]).format("DD-MM-YYYY")}</span>)
-            }
-        },
-        {
-            title: 'Tutor',
-            dataIndex: 'jobListing',
-            render: (record: any) => {
-                return (<span> {record.firstName + " " + record.lastName}</span>)
-            },
-        },
-        {
-            title: 'Hours/Session',
-            dataIndex: 'numHoursPerSession',
-            render: (numHoursPerSession: number) => {
-                return (
-                    <span>
-                        {numHoursPerSession}
-                    </span>
-                )
-            }
-        },
-        {
-            title: 'Additional Note',
-            dataIndex: 'additionalNote',
-            render: (notes: string) => {
-                return (
-                    <span>
-                    {notes}
-                </span>
-                )
-            }
-        },
-        {
-            title: 'No. of Sessions',
-            dataIndex: 'numSessions',
-        },
-        {
-            title: 'Offered Rate ($)',
-            dataIndex: 'offeredRate',
-            render: (rate: number) => {
-                return (<span>{rate} /hr</span>)
-            }
-        },
-        {
-            title: 'Rating',
-            dataIndex: 'rating',
-            render: (rating: any) => {
-                return (<span>{rating.ratingValue}</span>)
-            }
-        },
-        {
-            title: 'Action',
-            render: (record: any) => (
-                <div className="flex-row justify-center">
-                    <Button size="small" className="fs-12 bold" onClick={() => viewJobListing(record.jobListing.jobListingId)} type="primary">View JobListing</Button>
-                </div>
-            ),
-        }
-    ]
+	const _columns = [
+		{
+			title: "Offer Status",
+			dataIndex: "offerStatus",
+			render: (offerStatus: string) => {
+				const color =
+					offerStatus === "ACCEPTED"
+						? "success"
+						: offerStatus === "REJECTED"
+						? "error"
+						: offerStatus === "WITHDRAW"
+						? "default"
+						: "warning";
+				return (
+					<span>
+						<Tag color={color}>{offerStatus}</Tag>
+					</span>
+				);
+			},
+		},
+		{
+			title: "ID",
+			dataIndex: "offerId",
+		},
+		{
+			title: "Subject",
+			dataIndex: "chosenSubject",
+			render: (subject: any) => {
+				return <span>{subject.subjectName}</span>;
+			},
+		},
+		{
+			title: "Created Date",
+			dataIndex: "createdDate",
+			render: (date: string) => {
+				return <span>{moment(date.split("[")[0]).format("DD-MM-YYYY")}</span>;
+			},
+		},
+		{
+			title: "Tutor",
+			dataIndex: "jobListing",
+			render: (record: any) => {
+				return <span> {record.firstName + " " + record.lastName}</span>;
+			},
+		},
+		{
+			title: "Hours/Session",
+			dataIndex: "numHoursPerSession",
+			render: (numHoursPerSession: number) => {
+				return <span>{numHoursPerSession}</span>;
+			},
+		},
+		{
+			title: "Additional Note",
+			dataIndex: "additionalNote",
+			render: (notes: string) => {
+				return <span>{notes}</span>;
+			},
+		},
+		{
+			title: "No. of Sessions",
+			dataIndex: "numSessions",
+		},
+		{
+			title: "Offered Rate ($)",
+			dataIndex: "offeredRate",
+			render: (rate: number) => {
+				return <span>{rate} /hr</span>;
+			},
+		},
+		{
+			title: "Rating",
+			dataIndex: "rating",
+			render: (rating: any) => {
+				return <span>{rating ? rating.ratingValue : ""}</span>;
+			},
+		},
+		{
+			title: "Action",
+			render: (record: any) => (
+				<div className="flex-row justify-center">
+					<Button
+						size="small"
+						className="fs-12 bold"
+						onClick={() => viewJobListing(record.jobListing.jobListingId)}
+						type="primary"
+					>
+						View JobListing
+					</Button>
+				</div>
+			),
+		},
+	];
 
-    if(tableData.length === 0) {
-        return <div style={{marginTop: '20px', fontSize: '16px'}}>Loading...</div>
-    }
+	if (tableData.length === 0) {
+		return (
+			<div style={{ marginTop: "20px", fontSize: "16px" }}>Loading...</div>
+		);
+	}
 
-    // return (<div> in development</div>)
-    return (
-        <Table
-            columns={_columns}
-            dataSource={tableData}
-            scroll={{x: true}}
-            rowKey="offerId"
-        />
-    )
-}
+	// return (<div> in development</div>)
+	return (
+		<Table
+			columns={_columns}
+			dataSource={tableData}
+			scroll={{ x: true }}
+			rowKey="offerId"
+		/>
+	);
+};
 
 export default OfferListingTable;
--- frontend/src/Staff/components/StaffView/Tutee.tsx
+++ frontend/src/Staff/components/StaffView/Tutee.tsx
@@ -1,166 +1,165 @@
-import React, {useEffect, useState} from 'react';
-import {Button, message, Table, Tag} from "antd";
-import {TutorService} from "../../../services/Tutor";
-import moment from 'antd/node_modules/moment';
-import {StaffService} from "../../../services/Staff";
+import React, { useEffect, useState } from "react";
+import { Button, message, Table, Tag } from "antd";
+import moment from "antd/node_modules/moment";
+import { StaffService } from "../../../services/Staff";
 
 const TuteeTable = () => {
-    const [tableData, setTableData] = useState([])
-    const getAllTutors = async () => {
-        const data = await TutorService.getAllTutors();
-        setTableData(data)
-    }
-    useEffect(() => {
-        getAllTutors()
-    },[])
-    const updateTutorDetails = async(id: number, action: string) => {
-        let processed;
-        switch(action){
-            case "ban":
-                processed = await StaffService.banUser(id);
-                break;
+	const [tableData, setTableData] = useState([]);
+	const getAllTutees = async () => {
+		const data = await StaffService.getAllTutees();
+		setTableData(data);
+	};
+	useEffect(() => {
+		getAllTutees();
+	}, []);
+	const updateTuteeDetails = async (id: number, action: string) => {
+		let processed;
+		switch (action) {
+			case "ban":
+				processed = await StaffService.banUser(id);
+				break;
 
-            case "unBan":
-                processed = await StaffService.unBanUser(id);
-        }
+			case "unBan":
+				processed = await StaffService.unBanUser(id);
+		}
 
-        if(processed) {
-            getAllTutors();
-            message.success("Successfully updated user");
-            return true;
-        } else {
-            message.error("Unable to updated user");
-            return false;
-        }
-    }
+		if (processed) {
+			getAllTutees();
+			message.success("Successfully updated user");
+			return true;
+		} else {
+			message.error("Unable to updated user");
+			return false;
+		}
+	};
 
-    const banUser = (id: number) => {
-        updateTutorDetails(id, 'ban');
-    }
+	const banUser = (id: number) => {
+		updateTuteeDetails(id, "ban");
+	};
 
-    const unBanUser = (id: number) => {
-        updateTutorDetails(id, 'unBan');
-    }
+	const unBanUser = (id: number) => {
+		updateTuteeDetails(id, "unBan");
+	};
 
-    const viewProfile = (id: number) => {
-        console.log('viewProfile     =', id)
-    }
+	const viewProfile = (id: number) => {
+		console.log("viewProfile     =", id);
+	};
 
-    const _columns = [
-        {
-            title: 'Active Status',
-            dataIndex: 'activeStatus',
-            render: (activeStatus: boolean) => {
-                return (
-                    <span>
-                    {activeStatus?
-                        <Tag color="success">Active</Tag>
-                        :
-                        <Tag color="error">Inactive</Tag>
-                    }
-                </span>
-                )
-            }
-        },
-        {
-            title: 'ID',
-            dataIndex: 'personId',
-        },
-        {
-            title: 'First Name',
-            dataIndex: 'firstName',
-            sorter: (a: tutorDataType,b: tutorDataType) => {
-                if(a.firstName < b.firstName) { return -1; }
-                if(a.firstName > b.firstName) { return 1; }
-                return 0;
-            }
-        },
-        {
-            title: 'Last Name',
-            dataIndex: 'lastName',
-        },
-        {
-            title: 'Average Rating',
-            dataIndex: 'avgRating',
-        },
-        {
-            title: 'Citizenship',
-            dataIndex: 'citizenship',
-        },
-        {
-            title: 'CreatedDate',
-            dataIndex: 'createdDate',
-            render: (date: string) => {
-                const newD = moment(date.split('[')[0]);
-                return (
-                    <span>
-                        {newD.format('DD-MM-YYYY')}
-                    </span>
-                )
-            }
-        },
-        {
-            title: 'DOB',
-            dataIndex: 'dob',
-            render: (date: string) => {
-                const newD = moment(date.split('[')[0]);
-                return (
-                    <span>
-                        {newD.format('DD-MM-YYYY')}
-                    </span>
-                )
-            }
-        },
-        {
-            title: 'Email',
-            dataIndex: 'email',
-        },
-        {
-            title: 'Gender',
-            dataIndex: 'gender',
-        },
-        {
-            title: 'Highest Qualification',
-            dataIndex: 'highestQualification',
-        },
-        {
-            title: 'HP',
-            dataIndex: 'mobileNum',
-        },
-        {
-            title: 'Race',
-            dataIndex: 'race',
-        },
-        {
-            title: 'Rating Count',
-            dataIndex: 'ratingCount',
-        },
-        {
-            title: 'Action',
-            render: (record: tutorDataType) => (
-                <div className="flex-row justify-center">
-                    <Button size="small" className="fs-12 bold" onClick={() => viewProfile(record.personId)} type="primary">View</Button>
-                    {record .activeStatus?
-                        <Button size="small" className="fs-12 bold" onClick={() => banUser(record.personId)} type="primary" danger>DEACTIVATE</Button>
-                        :
-                        <Button size="small" className="fs-12 bold" onClick={() => unBanUser(record.personId)} type="default">REACTIVATE</Button>
-                    }
-                </div>
-            ),
-        }
-    ]
+	const _columns = [
+		{
+			title: "Active Status",
+			dataIndex: "activeStatus",
+			render: (activeStatus: boolean) => {
+				return (
+					<span>
+						{activeStatus ? (
+							<Tag color="success">Active</Tag>
+						) : (
+							<Tag color="error">Inactive</Tag>
+						)}
+					</span>
+				);
+			},
+		},
+		{
+			title: "ID",
+			dataIndex: "personId",
+		},
+		{
+			title: "First Name",
+			dataIndex: "firstName",
+			sorter: (a: tuteeDataType, b: tuteeDataType) => {
+				if (a.firstName < b.firstName) {
+					return -1;
+				}
+				if (a.firstName > b.firstName) {
+					return 1;
+				}
+				return 0;
+			},
+		},
+		{
+			title: "Last Name",
+			dataIndex: "lastName",
+		},
+		{
+			title: "CreatedDate",
+			dataIndex: "createdDate",
+			render: (date: string) => {
+				const newD = moment(date.split("[")[0]);
+				return <span>{newD.format("DD-MM-YYYY")}</span>;
+			},
+		},
+		{
+			title: "DOB",
+			dataIndex: "dob",
+			render: (date: string) => {
+				const newD = moment(date.split("[")[0]);
+				return <span>{newD.format("DD-MM-YYYY")}</span>;
+			},
+		},
+		{
+			title: "Email",
+			dataIndex: "email",
+		},
+		{
+			title: "Gender",
+			dataIndex: "gender",
+		},
+		{
+			title: "HP",
+			dataIndex: "mobileNum",
+		},
+		{
+			title: "Action",
+			render: (record: tuteeDataType) => (
+				<div className="flex-row justify-center">
+					<Button
+						size="small"
+						className="fs-12 bold"
+						onClick={() => viewProfile(record.personId)}
+						type="primary"
+					>
+						View
+					</Button>
+					{record.activeStatus ? (
+						<Button
+							size="small"
+							className="fs-12 bold"
+							onClick={() => banUser(record.personId)}
+							type="primary"
+							danger
+						>
+							DEACTIVATE
+						</Button>
+					) : (
+						<Button
+							size="small"
+							className="fs-12 bold"
+							onClick={() => unBanUser(record.personId)}
+							type="default"
+						>
+							REACTIVATE
+						</Button>
+					)}
+				</div>
+			),
+		},
+	];
 
-    if(tableData.length === 0) {
-        return <div style={{marginTop: '20px', fontSize: '16px'}}>Loading...</div>
-    }
-    return (
-        <Table
-            columns={_columns}
-            dataSource={tableData}
-            scroll={{x: true}}
-            rowKey="personId"
-        />
-    )
-}
+	if (tableData.length === 0) {
+		return (
+			<div style={{ marginTop: "20px", fontSize: "16px" }}>Loading...</div>
+		);
+	}
+	return (
+		<Table
+			columns={_columns}
+			dataSource={tableData}
+			scroll={{ x: true }}
+			rowKey="personId"
+		/>
+	);
+};
 
 export default TuteeTable;
